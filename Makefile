54ebf991e80845e1c371d65a73e95cc6c6d8cca6
kdecara <greenarrowkd07@gmail.com>
2021-12-29 19:45:42 -0500

    euclidean algorithm added, attacks added

--- Makefile
+++ Makefile
@@ -1,22 +1,74 @@
 
+#define variables to refer to respective directories
+SRCDIR = ./src
+#OBJDIR = ./obj
+#BINDIR = ./bin
+INCDIR = ./inc
+
+#explicitly define the SRCS variable, which consists of all of our source files
+SRCS = $(SRCDIR)/affine_cipher.c $(SRCDIR)/file_pack.c
+
+#define the INCLUDE variable, which consists of all of our header files
+INCLUDE = $(addprefix -I, $(INCDIR))
+#define the OBJS variable from the SRCS variable
+#OBJS = $(SRCS:$(SRCDIR)/%.c=$(OBJDIR)/%.o)
+OBJS = $(SRCS:$(SRCDIR)/%.c=%.o)
+
 CC = gcc
-CFLAGS = -Wall -g -std=c89 -pedantic-errors
+#CFLAGS variable, note that $(INCLUDE) is in here, so we don't need to declare in the recipes below
+CFLAGS = $(INCLUDE) -Wall -g -std=c99 -pedantic-errors
+
+#our file executable will be name des
+#TARGET = $(BINDIR)/des
 
 TARGET = affine_cipher
 
+#make will go to this target when called
 all: $(TARGET)
 
-$(TARGET): $(TARGET).c file_pack.c
-	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c file_pack.c
+#Recipe for the target, compile from OBJS variable
+$(TARGET): $(OBJS)
+	$(CC) $(CFLAGS) -o $@ $(OBJS) 
 
+#compile each OBJS variable from its respective source file and header file
+#$(OBJS): $(OBJDIR)/%.o : $(SRCDIR)/%.c 
+#	$(CC) $(CFLAGS) -c $< -o $@
+
+$(OBJS): %.o : $(SRCDIR)/%.c 
+	$(CC) $(CFLAGS) -c $< -o $@
+
+#clean will remove the compiled object files and executable
 clean:
+	rm -f $(OBJS)
 	rm $(TARGET)
 
-#test: $(TARGET).c
-#	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c
-#	./#NAME OF YOUR PROGRAM tfile.txt packed.out
+#the executable must be compiled from each object file
+#each object file must be compiled with its equivalent source file and header file as arguments (header included
+#in source files)
+#each source file must be compiled with its equivalent header file as arguments (perhaps not needed???)
+
+#teste: $(TARGET).c file_pack.c byte_pack.c key.c
+#	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c file_pack.c block.c byte_pack.c key.c
+#	./des tfile.txt out.enc 0xAAAAA e
 #	ls -l tfile.txt
-#	ls -l packed.out
+#	ls -l out.enc
+#	cat out.enc
+#	hexdump out.enc
+
+#testd: $(TARGET).c file_pack.c byte_pack.c key.c
+#	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c file_pack.c block.c byte_pack.c key.c
+#	./des out.enc out.dec 0xAAAAA d
+#	ls -l tfile.txt
+#	ls -l out.dec
+#	cat out.dec
+#	hexdump tfile.txt
+#	hexdump out.enc
+#	hexdump out.dec
+
+#debug: $(TARGET).c file_pack.c byte_pack.c
+#	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c file_pack.c block.c byte_pack.c
+#	gdb ./des tfile.txt out.enc 0xAAAAA e
 
 #val:
-#	valgrind --leak-check=yes ./text_compression compress tfile.txt tbinary.bin
+#	valgrind --leak-check=yes ./des tfile.txt out.enc 0xAAAAA e
+#	valgrind --leak-check=yes ./des tfile.txt out.enc 0xAAAAA d
--- src/affine_cipher.c
+++ src/affine_cipher.c
@@ -6,7 +6,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "../file_pack.h"
+#include "file_pack.h"
 
 /*in C, the % operator returns the result remainder, and not necessarily the modulo
  *so it is necessary to write our own mod function. Since this will be used quite frequently I've made
@@ -23,6 +23,8 @@
 void encrypt(FILE *in, FILE *out, int a, int b);
 void decrypt(FILE *in, FILE *out, int a, int b);
 int brute_force_inverse(int a);
+int gcd_euclid(int a, int b);
+int euclid_extended(int a, int b, int *x, int *y);
 
 int main(int argc, char *argv[])
 {
@@ -31,7 +33,6 @@ int main(int argc, char *argv[])
     int a = atoi(argv[3]);
     int b = atoi(argv[4]);
     char *mode = argv[5];
-     
     if(mode[0] == 'e') encrypt(in_file->fp, out_file->fp, a, b);
     else if(mode[0] == 'd') decrypt(in_file->fp, out_file->fp, a, b);
     
@@ -40,6 +41,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/*takes every character from in_file and applies the cipher, and places it in outfile*/
 void encrypt(FILE *in, FILE *out, int a, int b)
 {
     char c;
@@ -47,24 +49,26 @@ void encrypt(FILE *in, FILE *out, int a, int b)
     printf("encrypting file...\n");
     while(c != EOF)
     {
-	c = MOD((c * a + b), RING_SIZE);
-	fputc(c, out);
-	c = fgetc(in);
+	    c = MOD((c * a + b), RING_SIZE);
+	    fputc(c, out);
+	    c = fgetc(in);
     }
     printf("file encrypted\n");
 }
 
+/*takes every character from infile, undoes the cipher, and places it in outfile*/
 void decrypt(FILE *in, FILE*out, int a, int b)
 {
     char c;
-    int inv_a = brute_force_inverse(a);
+    int inv_a = brute_force_inverse(a), y;
+    euclid_extended(a, RING_SIZE, &inv_a, &y);
     c = fgetc(in);
     printf("decrypting file...\n");
     while(c != EOF)
     {
-	c = MOD((inv_a * (c - b)), RING_SIZE);
-	fputc(c, out);
-	c = fgetc(in);
+	    c = MOD((inv_a * (c - b)), RING_SIZE);
+	    fputc(c, out);
+	    c = fgetc(in);
     }
     printf("file decrypted\n");
 }
@@ -76,21 +80,64 @@ void decrypt(FILE *in, FILE*out, int a, int b)
  */
 int brute_force_inverse(int a)
 {
-    int inverse, c;
+    int inverse = - 1, c;
     for(c = 0; c < 128; c++)
     {
-	if(a * c % 128 == 1)
-	{
-	    inverse = c; 
-	    break;
-	}
+	    if(a * c % 128 == 1)
+	    {
+	        inverse = c; 
+	        break;
+	    }
     }
     printf("the inverse of %d is %d\n", a, inverse);
     return inverse;
 }
 
-/*why are some letters not being decrypyed properly? we can start with the most common occurence noted
- *the letter e*/
+/*the euclidean algorithm is a more efficient way to find the greatest common divisor of two integers x and y
+ * more efficient than brute force, it returns the g = gcd(a, b)
+ *
+ * The algorithm is as follows: (note that b in our case is RING_SIZE of 128
+ * if a < b switch a and b (does not apply to us since the RING_SIZE = 128 is our b)
+ * R = remainder of a/b (a%b in our case, more specicially MOD(a, b))
+ * a = b and b = r, repeat previous step
+ * if the gcd of a and RING_SIZE is not 1, then a
+ *
+ * For use in this program, if the GCD(a, RING_SIZE=128) = 1, we can say that a has an inverse
+ * in the integer ring RING_SIZE=128. This algorithm can be extended to find the inv_a in RING_SIZE
+ * In a way that is more efficient than brute force*/
+int gcd_euclid(int a, int b)
+{
+    int r = MOD(a, b), gcd;
+    if(r == 0) return b;
+    gcd = gcd_euclid(b, r);
+    return gcd;
+}
+
+/*
+ * The extended euclidean algorithm is a method for for finding inv_a in the ring b (in our case 128)
+ * it returns the gcd of a and b, which we know is 1 - this function is called with the assumption the inv
+ * of a exists in b.
+ * the inv_a is stored in x
+ * more efficient than brute force, this function can tell us if the inv_a exists in the given integer ring
+ * and if it does, what inv_a is
+ */
+int euclid_extended(int a, int b, int *x, int *y)
+{
+    int x1, y1, gcd, r;
+    if(a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    r = MOD(b, a);
+    gcd = euclid_extended(r, a, &x1, &y1);
+    *x = y1 - (b/a) * x1;
+    *y = x1;
+    return gcd;
+}
+
+/*test to see if the functions are working properly:
 void test(FILE* fp)
 {
     int a = 5, b = 22, modulus = 128, inv_a = brute_force_inverse(a), enc_c, dec_c;
@@ -100,10 +147,11 @@ void test(FILE* fp)
     {
         enc_c = MOD((a * c + b), modulus);
         dec_c = MOD(inv_a * (enc_c - b), modulus);
-	printf("c = %c = %d\tenc_c = %c = %d\tdec_c = %c = %d\t\n", c, c, enc_c, enc_c, dec_c, dec_c);
-	c = fgetc(fp);
+	    printf("c = %c = %d\tenc_c = %c = %d\tdec_c = %c = %d\t\n", c, c, enc_c, enc_c, dec_c, dec_c);
+	    c = fgetc(fp);
     }
     printf("test done\n");
 }
+*/
 
 
